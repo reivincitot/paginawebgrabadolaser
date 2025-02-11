a40b3ea2bb8c2a3fd816ff223060548b6ba82607
sebastian <reivincitot@gmail.com>
2025-02-11 01:36:05 -0300

    Definicion del modelo products y creacion de la app users

---
+++ backend/products/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    images = models.ImageField(upload_to='img/products')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    materials = models.JSONField(default=list)
+    dimensions = models.CharField(max_length=100)
+    disponibility = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
