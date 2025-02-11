80264b69091e6e0d7e7aafe456b9e5cfc47a221a
sebastian <reivincitot@gmail.com>
2025-02-11 02:03:14 -0300

    Definición del modelo users

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """Extend Django user model to add custom fields."""
+
+    phone_number = models.CharField(max_length=15, blank=True)
+    is_cliente = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
+
+
+class Client(models.Model):
+    """Model to represent a client."""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    direcction = (models.CharField(blank=True),)
+    preferred_lenguage = models.CharField(
+        max_length=10, choices=[("es", "Spanish"), ("en", "English")], default="es"
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    charge = models.CharField(
+        max_length=10,
+        choices=[
+            ("admin", "Admin"),
+            ("seller", "Seller"),
+        ],
+    )
+    def __str__(self):
+        return f"(self.user.username) - (self.charge)"
